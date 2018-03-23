becc4be6ab8ad09b110820b03348095af1e3de2f
IvanWhisper <Ivan_Whisper@outlook.com>
2018-03-23 17:44:37 +0800

    修正

--- SQLUserCreate/Program.cs
+++ SQLUserCreate/Program.cs
@@ -16,12 +16,12 @@ namespace SQLUserCreate
             var cuser3sql = "GRANT USAGE ON *.* TO 'test3'@'localhost' IDENTIFIED BY '1234' WITH GRANT OPTION";
             var cuser4sql = "GRANT USAGE ON *.* TO 'test4'@'localhost' IDENTIFIED BY '1234' WITH GRANT OPTION";
             //授权
-            var guser1sql = "GRANT SELECT ON yundaclient.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
-            var guser2sql = "GRANT SELECT, INSERT ON yundaclient.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
-            var guser3sql = "GRANT SELECT, INSERT, UPDATE ON yundaclient.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
-            var guser4sql = "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP ON yundaclient.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
+            var guser1sql = "GRANT SELECT ON client.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
+            var guser2sql = "GRANT SELECT, INSERT ON client.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
+            var guser3sql = "GRANT SELECT, INSERT, UPDATE ON client.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
+            var guser4sql = "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP ON client.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
             //回收权限
-            var ruser1sql= "revoke all ON yundaclient.* from 'test1' @'localhost'";
+            var ruser1sql= "revoke all ON client.* from 'test1' @'localhost'";
             Console.WriteLine(MySqlHelper.ExecuteNonQuery(ruser1sql));
             //建用户
             //Console.WriteLine(MySqlHelper.ExecuteNonQuery(cuser1sql));
