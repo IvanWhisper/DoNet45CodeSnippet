bc0dc003f8fb63332ec2a5c55fc4c2a8b917899d
IvanWhisper <Ivan_Whisper@outlook.com>
2018-03-23 17:38:11 +0800

    添加了webp格式图片转化Demo&数据库管理员权限创建与授权

---
+++ SQLUserCreate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLUserCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //建用户
+            var cuser1sql = "GRANT USAGE ON *.* TO 'test1'@'localhost' IDENTIFIED BY '1234' WITH GRANT OPTION";
+            var cuser2sql = "GRANT USAGE ON *.* TO 'test2'@'localhost' IDENTIFIED BY '1234' WITH GRANT OPTION";
+            var cuser3sql = "GRANT USAGE ON *.* TO 'test3'@'localhost' IDENTIFIED BY '1234' WITH GRANT OPTION";
+            var cuser4sql = "GRANT USAGE ON *.* TO 'test4'@'localhost' IDENTIFIED BY '1234' WITH GRANT OPTION";
+            //授权
+            var guser1sql = "GRANT SELECT ON yundaclient.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
+            var guser2sql = "GRANT SELECT, INSERT ON yundaclient.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
+            var guser3sql = "GRANT SELECT, INSERT, UPDATE ON yundaclient.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
+            var guser4sql = "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP ON yundaclient.*TO 'test1'@'localhost' IDENTIFIED BY '1234'";
+            //回收权限
+            var ruser1sql= "revoke all ON yundaclient.* from 'test1' @'localhost'";
+            Console.WriteLine(MySqlHelper.ExecuteNonQuery(ruser1sql));
+            //建用户
+            //Console.WriteLine(MySqlHelper.ExecuteNonQuery(cuser1sql));
+            //Console.WriteLine(MySqlHelper.ExecuteNonQuery(cuser2sql));
+            //Console.WriteLine(MySqlHelper.ExecuteNonQuery(cuser3sql));
+            //Console.WriteLine(MySqlHelper.ExecuteNonQuery(cuser4sql));
+            //授权
+            //Console.WriteLine(MySqlHelper.ExecuteNonQuery(guser1sql));
+            //Console.WriteLine(MySqlHelper.ExecuteNonQuery(guser2sql));
+            //Console.WriteLine(MySqlHelper.ExecuteNonQuery(guser3sql));
+            //Console.WriteLine(MySqlHelper.ExecuteNonQuery(guser4sql));
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ WebPDemo/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPDemo
+{
+    class Program
+    {
+        static Stopwatch sw = new Stopwatch();
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@".\pic\");
+            FileSystemInfo[] dirs = d.GetFileSystemInfos();
+            foreach (var item in dirs)
+            {
+                try {
+                    long a = System.Environment.TickCount;
+                    //var str=encodeWebP(Image.FromFile(item.FullName),item.FullName);
+                    var str = encodeNormal(Image.FromFile(item.FullName), item.FullName);
+                    a = System.Environment.TickCount - a;
+                    Console.WriteLine(str+"。。。"+a);
+                }
+                catch
+                {
+
+                }
+            }
+            //Console.WriteLine(f.CreationTime);
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// 将实际位置中的照片转化为byte[]类型写入数据库中
+        /// </summary>
+        /// <param name="strFile">string图片地址</param>
+        /// <returns>byte[]</returns>
+        public static byte[] GetBytesByImagePath(string strFile)
+        {
+            byte[] photo_byte = null;
+            using (FileStream fs =
+            new FileStream(strFile, FileMode.Open, FileAccess.Read))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    photo_byte = br.ReadBytes((int)fs.Length);
+                }
+            }
+
+            return photo_byte;
+        }
+        public static string encodeWebP(Image pic,string name)
+        {
+            sw.Restart();
+            var bmp = new Bitmap(pic.Width, pic.Height, PixelFormat.Format32bppArgb);
+            var getImg = sw.Elapsed.Milliseconds;
+            using (var g = Graphics.FromImage(bmp))
+            {
+                g.DrawImage(pic, 0, 0, pic.Width, pic.Height);
+            }
+            var cgImg = sw.ElapsedMilliseconds;
+            using (FileStream fs = System.IO.File.Create(name.Replace("pic","out")+".webp"))
+            {
+                new Imazen.WebP.SimpleEncoder().Encode(bmp, fs, 75);
+            }
+            GC.Collect();
+            var webp = sw.ElapsedMilliseconds;
+            return string.Format("耗时，提取{0}ms，转化{1}ms，压缩成webp{2}ms，总计{3}ms",getImg,cgImg-getImg,webp-cgImg,webp);
+        }
+        public static string encodeNormal(Image pic, string name)
+        {
+            sw.Restart();
+            var bmp = new Bitmap(pic.Width, pic.Height, PixelFormat.Format32bppArgb);
+            var getImg = sw.Elapsed.Milliseconds;
+            using (var g = Graphics.FromImage(bmp))
+            {
+                g.DrawImage(pic, 0, 0, pic.Width, pic.Height);
+            }
+            var cgImg = sw.ElapsedMilliseconds;
+            ImgResize.GetPicThumbnail(bmp, name.Replace("pic", "outpng") + ".jpg",75);
+            GC.Collect();
+            var webp = sw.ElapsedMilliseconds;
+            return string.Format("耗时，提取{0}ms，转化{1}ms，压缩成算法{2}ms，总计{3}ms", getImg, cgImg - getImg, webp - cgImg, webp);
+        }
+    }
+}
